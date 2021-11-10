3d526220ccd953943f3a1717e82ce47226ccc96b
gouthamkishore <send2kgk3@gmail.com>
2021-11-10 13:37:18 +0530

    changes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import './Sidebar.css'
-import ChatIcon from '@mui/icons-material/Chat';
-import DonutLargeIcon from '@mui/icons-material/DonutLarge';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ChatIcon from '@material-ui/icons/Chat';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { IconButton } from '@material-ui/core';
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
                 <div className="sidebar__headerRight">
-                    <ChatIcon />
-                    <IconButton>
-                        <DonutLargeIcon />
-                    </IconButton>
+                    <IconButton><ChatIcon /></IconButton>
+                    <IconButton><DonutLargeIcon /></IconButton>
+                    <IconButton><MoreVertIcon /></IconButton>
 
-                    <MoreVertIcon />
+
+
+
                 </div>
             </div>
         </div>
     )
 }
+
+
 
 export default Sidebar
